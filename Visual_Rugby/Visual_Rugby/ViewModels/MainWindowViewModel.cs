d50373ab6c0fff8cf3d1abea3730046b9833ccaa
Nikita <nikitatorok002@gmail.com>
2022-05-28 10:38:52 +0700

    Stage 6

--- Visual_Rugby/Visual_Rugby/ViewModels/MainWindowViewModel.cs
+++ Visual_Rugby/Visual_Rugby/ViewModels/MainWindowViewModel.cs
@@ -1,32 +1,26 @@
-using Avalonia.Media;
-using Avalonia;
 using ReactiveUI;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Xml.Serialization;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Data.Sqlite;
 using Visual_Rugby.Models;
-using System.Collections.Specialized;
 
 
 namespace Visual_Rugby.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        
+        private ObservableCollection<Team> _teams;
+        private ObservableCollection<Result> _results;
+        private ObservableCollection<Staduim> _staduims;
+        private ObservableCollection<Tournament> _tournaments;
+        private ObservableCollection<Match> _matchs;
+        private ObservableCollection<Country> _countrys;
+        rugbyContext data;
+        public rugbyContext Data
+        {
+            get { return data; }
+            set { this.RaiseAndSetIfChanged(ref data, value); }
+        }                                    
        
-        public ObservableCollection<Team> Teams { get; set; }
-        public ObservableCollection<Result> Results { get; set; }
-        public ObservableCollection<Staduim> Staduims { get; set; }
-        public ObservableCollection<Tournament> Tournaments { get; set; }
-        public ObservableCollection<Match> Matchs { get; set; }
-        public ObservableCollection<Country> Countrys { get; set; }
-
         private ViewModelBase _content;
         public ViewModelBase Content
         {
@@ -35,22 +29,58 @@ namespace Visual_Rugby.ViewModels
 
         }
         
+        public ObservableCollection<Team> Teams
+        {
+            get => _teams;
+            set => this.RaiseAndSetIfChanged(ref _teams, value);
+        }
+
+        public ObservableCollection <Result> Results
+        {
+            get => _results;
+            set => this.RaiseAndSetIfChanged(ref _results, value);
+        }
+
+        public ObservableCollection<Staduim> Staduims
+        {
+            get => _staduims;
+            set => this.RaiseAndSetIfChanged(ref _staduims, value);
+        }
+
+        public ObservableCollection<Tournament> Tournaments
+        {
+            get => _tournaments;
+            set => this.RaiseAndSetIfChanged(ref _tournaments, value);
+        }
+
+        public ObservableCollection<Match> Matches
+        {
+            get => _matchs;
+            set => this.RaiseAndSetIfChanged(ref _matchs, value);
+        }
+
+        public ObservableCollection<Country> Countries
+        {
+            get => _countrys;
+            set => this.RaiseAndSetIfChanged(ref _countrys, value);
+        }
+
         public MainWindowViewModel()
         {
             Teams = new ObservableCollection<Team>();
             Results = new ObservableCollection<Result>();
             Staduims = new ObservableCollection<Staduim>();
             Tournaments = new ObservableCollection<Tournament>();
-            Matchs = new ObservableCollection<Match>();
-            Countrys = new ObservableCollection<Country>();
+            Matches = new ObservableCollection<Match>();
+            Countries = new ObservableCollection<Country>();
             using(var db = new rugbyContext())
             {
                 foreach (var team in db.Teams) Teams.Add(team);
                 foreach (var result in db.Results) Results.Add(result);
                 foreach (var staduim in db.Staduims) Staduims.Add(staduim);
                 foreach (var tournament in db.Tournaments) Tournaments.Add(tournament);
-                foreach (var match in db.Matches) Matchs.Add(match);
-                foreach (var country in db.Countries) Countrys.Add(country);
+                foreach (var match in db.Matches) Matches.Add(match);
+                foreach (var country in db.Countries) Countries.Add(country);
             }
             Content = new DataTableViewModel();
             
@@ -59,34 +89,49 @@ namespace Visual_Rugby.ViewModels
         public void DeleteRowTeam(Team team)
         {
             Teams.Remove(team);
-            Content = new DataTableViewModel();
+         
         }
         public void DeleteRowResult(Result result)
         {
             Results.Remove(result);
-            Content = new DataTableViewModel();
+            
         }
         public void DeleteRowMatch(Match match)
         {
-            Matchs.Remove(match);
-            Content = new DataTableViewModel();
+            Matches.Remove(match);
+            
         }
         public void DeleteRowCountry(Country country)
         {
-            Countrys.Remove(country);
-            Content = new DataTableViewModel();
+            Countries.Remove(country);
+            
         }
         public void DeleteRowStaduim(Staduim staduim)
         {
             Staduims.Remove(staduim);
-            Content = new DataTableViewModel();
+            
         }
         public void DeleteRowTournament(Tournament tournament)
         {
             Tournaments.Remove(tournament);
-            Content = new DataTableViewModel();
         }
         
+
+        private void NewRowTeam() => Teams.Add(new Team());
+        private void NewRowResult() => Results.Add(new Result());
+        private void NewRowCountry() => Countries.Add(new Country());
+        private void NewRowMatch() => Matches.Add(new Match());
+        private void NewRowStaduim() => Staduims.Add(new Staduim());
+        private void NewRowTournament() => Tournaments.Add(new Tournament());
+        private void SaveChangesToBD()
+        {
+            var db = new rugbyContext();
+            db.SaveChanges();
+        }
+        private void GoToMainMenu()
+        {
+            Content = new DataTableViewModel();
+        }
         public void RequestToDB()
         {
             Content = new RequestViewModel();
